df85ddf63dbb1030ef92eaba896eb1fb8f659cea
Honuluau <honu.dylan@gmail.com>
2024-10-21 15:44:49 -0400

    First Commit

---
+++ src/main/java/net/honuluau/coinflip/Coinflip.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package net.honuluau.coinflip;
+
+/**
+ *
+ * @author dd18787
+ */
+public class Coinflip {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
